b32a8147ba7ba5e54a73d238afb38efd92472653
Micheal McErlean <MichealMcErlean@users.noreply.github.com>
2025-11-14 11:11:23 +1100

    Split webpack.config.js for prod and dev

---
+++ webpack.common.js
@@ -0,0 +1,21 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/template.html",
+    }),
+  ],
+  "scripts": {
+    "build": "webpack",
+    "dev": "webpack serve",
+    "deploy": "git subtree push --prefix dist origin gh-pages"
+  },
+};
--- webpack.config.js
+++ webpack.config.js
@@ -14,8 +14,8 @@ module.exports = {
     watchFiles: ["./src/template.html"],
   },
   "scripts": {
-    "build": "webpack",
-    "dev": "webpack serve",
+    "build": "webpack --config webpack.prod.js",
+    "dev": "webpack serve --config webpack.dev.js",
     "deploy": "git subtree push --prefix dist origin gh-pages"
   },
   plugins: [
